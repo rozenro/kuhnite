7893ec18f22f6b8f2b1c9211a7a4768b59e9c773
rozenro <rozenov@tuta.io>
2025-07-14 14:09:31 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -165,7 +165,7 @@ function App() {
                   </div>
                   <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">
-                      Имейл <span className="text-blue-800 text-xs font-normal">/препоръчително, ако качвате файл към запитването/</span>
+                      Имейл <span className="text-blue-800 text-xs font-normal">/препоръчително, и ако качвате файл към запитването/</span>
                     </label>
                     <input
                       type="email"
